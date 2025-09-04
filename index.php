3c76b6e2b786182a4c78b7170ed701f9664d8fee
h1101241039-cmd <h1101241039@student.untan.ac.id>
2025-09-04 11:32:51 +0700

    index

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Halaman Belajar Wenny</title>
+    <title>Halaman Belajar ku</title>
     <style>
         body {
             font-family: Arial, sans-serif;
